dfe2a16df1bfb0123ecc7db33b4d4f7fb7845c4c
Martin Frichitthavong <frichitthavongmartin@gmail.com>
2020-04-25 20:43:51 -0400

    Add project files.

---
+++ Unit Coversion/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Unit_Coversion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.LinearConversion = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.chooseBox = new System.Windows.Forms.TextBox();
+            this.unitBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LinearConversion
+            // 
+            this.LinearConversion.AutoSize = true;
+            this.LinearConversion.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.LinearConversion.Font = new System.Drawing.Font("MV Boli", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LinearConversion.ForeColor = System.Drawing.Color.Crimson;
+            this.LinearConversion.Location = new System.Drawing.Point(126, 39);
+            this.LinearConversion.Name = "LinearConversion";
+            this.LinearConversion.Size = new System.Drawing.Size(246, 37);
+            this.LinearConversion.TabIndex = 0;
+            this.LinearConversion.Text = "Linear Conversion";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MV Boli", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(47, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(250, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "1. Inches To Centimetres";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MV Boli", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(198, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(242, 26);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "2. Feet To Centimetres";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MV Boli", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(47, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(205, 26);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "3. Yards To Metres";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MV Boli", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(198, 193);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(234, 26);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "4. Miles To Kilometres";
+            // 
+            // chooseBox
+            // 
+            this.chooseBox.Location = new System.Drawing.Point(331, 279);
+            this.chooseBox.Name = "chooseBox";
+            this.chooseBox.Size = new System.Drawing.Size(100, 26);
+            this.chooseBox.TabIndex = 5;
+            // 
+            // unitBox
+            // 
+            this.unitBox.Location = new System.Drawing.Point(331, 321);
+            this.unitBox.Name = "unitBox";
+            this.unitBox.Size = new System.Drawing.Size(100, 26);
+            this.unitBox.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MV Boli", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(159, 279);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(138, 26);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Choose (1-4)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("MV Boli", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(32, 319);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(293, 26);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Enter Value to Be Converted";
+            // 
+            // convertButton
+            // 
+            this.convertButton.Location = new System.Drawing.Point(153, 366);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(162, 39);
+            this.convertButton.TabIndex = 9;
+            this.convertButton.Text = "Convert";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(78, 428);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(0, 20);
+            this.outputLabel.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(497, 547);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.unitBox);
+            this.Controls.Add(this.chooseBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LinearConversion);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Unit Coversion";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LinearConversion;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox chooseBox;
+        private System.Windows.Forms.TextBox unitBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ Unit Coversion/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Unit_Coversion
+{
+    public partial class Form1 : Form
+    {
+
+        double choice, unit, answer; 
+        
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            choice = Convert.ToDouble(chooseBox.Text); 
+            if (choice == 1)
+            {
+                unit = Convert.ToDouble(unitBox.Text);
+                InchesToCm();
+            }
+            else if (choice == 2)
+            {
+                unit = Convert.ToDouble(unitBox.Text);
+                FeetToCm();
+            }
+            else if (choice == 3)
+            {
+                unit = Convert.ToDouble(unitBox.Text);
+                YardsToMetres();
+            }
+            else if (choice == 4)
+            {
+                unit = Convert.ToDouble(unitBox.Text);
+                MilesToKm();
+            }
+        }
+
+        public void InchesToCm()
+        {
+            answer = unit * 2.54;
+            outputLabel.Text = unit + " inches = " + answer.ToString("0.00 cm"); 
+        }
+
+        public void FeetToCm()
+        {
+            answer = unit * 30.48;
+            outputLabel.Text = unit + " Feet = " + answer.ToString("0.00 cm");
+        }
+
+        public void YardsToMetres()
+        {
+            answer = unit * 0.91;
+            outputLabel.Text = unit + " Yard = " + answer.ToString("0.00 Metres");
+        }
+
+        public void MilesToKm()
+        {
+            answer = unit * 1.6;
+            outputLabel.Text = unit + " Miles = " + answer.ToString("0.00 Kilometres");
+        }
+
+    }
+}
